3271bb951acf616c9832b8752d65e29283388b15
sgandru <sambasivarao.gandru@zenmonics.com>
2019-01-03 20:38:30 +0530

    updated code to use domain model object

--- src/app/jokeList.component.ts
+++ src/app/jokeList.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+class Joke {
+  heading : String;
+  tagLine : String;
+  hide : boolean;
+  constructor(heading: String, tagLine: String){
+      this.heading = heading;
+      this.tagLine = tagLine;
+      this.hide = true;
+  }
+
+  toggle() {
+      this.hide = !this.hide;
+  }
+}
+
 @Component({
   selector: 'joke-list',
   templateUrl: './jokeList.component.html',
@@ -9,25 +24,13 @@ export class JokeComponentList {
   //heading : String;
   //tagLine : String;
   title = 'Jokes List';
-  jokeList : Object[];
+  jokeList : Joke[];
 
   constructor(){
     this.jokeList = [
-        {
-          heading: "What did the cheese say when it looked in the mirror?",
-          tagLine: "Hello-Me (Halloumi)",
-          hide: "true"
-        },
-        {
-          heading: "What kind of cheese do you use to disguise a small horse?",
-          tagLine: "Mask-a-pony (Mascarpone)",
-          hide: "true"
-        },
-        {
-          heading: "A kid threw a lump of cheddar at me",
-          tagLine: "I thought ‘That’s not very mature’",
-          hide: "true"
-        }
+      new Joke("What did the cheese say when it looked in the mirror?", "Hello-Me (Halloumi)"),
+      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
     ];
   }
 }
